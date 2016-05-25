1361fdf99c7fb09de0aa004ec2534a21f3294a59
alloywheels <turrifftyresandalloys@gmail.com>
2016-05-25 14:18:26 +0100

    finished smart title to test

--- wheeltyretitle.php
+++ wheeltyretitle.php
@@ -73,7 +73,7 @@ $barecarmodel = $row["MODEL"];
 
 //tyre details - from BestFitTyres
 $bestfittyre = $row["BestFitTyres"];
-
+$bestfittyre = $bestfittyre.' ';
 
 //pretitle first
 
@@ -129,21 +129,141 @@ if ($lenght > 80){
 	
 	$title = $pretitlef.'FIT > '.$cardet;
 	
-	} elseif (($lenght >= 48) && ($lenght <= 52)) {  // 28 characters >>> 52
+	} elseif ($lenght == 73) { // 7 characters >>> 73
 	
-	$title = $pretitlef.'AND '.$bestfittyre.' TYRES FIT '.$cardet;
+	$title = $pretitlef.'FIT -> '.$cardet;
 	
-	} elseif (($lenght >= 44) && ($lenght <= 47)) {  // 33 characters >>> 47
+	} elseif ($lenght == 72) { // 8 characters >>> 72
 	
-	$title = $pretitlef.' RIMS AND'.$bestfittyre.' TYRES FIT '.$cardet;
+	$title = $pretitlef.'FITS -> '.$cardet;
 	
-	} elseif (($lenght >= 42) && ($lenght <= 43)) {  // 37 characters >>> 43
+	} elseif ($lenght == 71) { // 9 characters >>> 71
 	
-	$title = $pretitlef.' WHEELS AND '.$bestfittyre.' TYRES FIT '.$cardet;
+	$title = $pretitlef.'FITS --> '.$cardet;
 	
-	} elseif (($lenght >= 20) && ($lenght <= 41)) {  // 39 characters >>> 41
+	} elseif ($lenght == 70) { // 10 characters >>> 70
 	
-	$title = $pretitlef.'ALLOY WHEELS AND '.$bestfittyre.' TYRES FIT '.$cardet;
+	$title = $pretitlef.'& TYRES > '.$cardet;
+	
+	} elseif ($lenght == 69) { // 11 characters >>> 69
+	
+	$title = $pretitlef.'& TYRES -> '.$cardet;
+	
+	} elseif ($lenght == 68) { // 12 characters >>> 68
+	
+	$title = $pretitlef.'& TYRES FIT '.$cardet;
+	
+	} elseif ($lenght == 67) { // 13 characters >>> 67
+	
+	$title = $pretitlef.'& TYRES FITS '.$cardet;
+	
+	} elseif ($lenght == 66) { // 14 characters >>> 66
+	
+	$title = $pretitlef.'& TYRES FIT > '.$cardet;
+	
+	} elseif ($lenght == 65) { // 15 characters >>> 65
+	
+	$title = $pretitlef.'& TYRES FIT -> '.$cardet;
+	
+	} elseif ($lenght == 64) { // 16 characters >>> 64
+	
+	$title = $pretitlef.'& TYRES FITS -> '.$cardet;
+	
+	} elseif ($lenght == 63) { // 17 characters >>> 63
+	
+	$title = $pretitlef.'AND TYRES FITS > '.$cardet;
+	
+	} elseif ($lenght == 62) { // 18 characters >>> 62
+	
+	$title = $pretitlef.'AND TYRES FITS -> '.$cardet;
+	
+	} elseif ($lenght == 61) { // 19 characters >>> 61
+	
+	$title = $pretitlef.'& '.$bestfittyre.'TYRES- '.$cardet;
+	
+	} elseif ($lenght == 60) { // 20 characters >>> 60
+	
+	$title = $pretitlef.'& '.$bestfittyre.'TYRES > '.$cardet;
+	
+	} elseif ($lenght == 59) { // 21 characters >>> 59
+	
+	$title = $pretitlef.'& '.$bestfittyre.'TYRES -> '.$cardet;
+	
+	} elseif ($lenght == 58) { // 22 characters >>> 58
+	
+	$title = $pretitlef.'& '.$bestfittyre.'TYRES FIT '.$cardet;
+	
+	} elseif ($lenght == 57) { // 23 characters >>> 57
+	
+	$title = $pretitlef.'AND '.$bestfittyre.'TYRES -> '.$cardet;
+	
+	} elseif ($lenght == 56) { // 24 characters >>> 56
+	
+	$title = $pretitlef.'AND '.$bestfittyre.'TYRES FIT '.$cardet;
+	
+	} elseif ($lenght == 55) { // 25 characters >>> 55
+	
+	$title = $pretitlef.'AND '.$bestfittyre.'TYRES FITS '.$cardet;
+	
+	} elseif ($lenght == 54) { // 26 characters >>> 54
+	
+	$title = $pretitlef.'RIMS AND '.$bestfittyre.'TYRES- '.$cardet;
+	
+	} elseif ($lenght == 53) { // 27 characters >>> 53
+	
+	$title = $pretitlef.'RIMS AND '.$bestfittyre.'TYRES > '.$cardet;
+	
+	} elseif ($lenght == 52) { // 28 characters >>> 52
+	
+	$title = $pretitlef.'RIMS AND '.$bestfittyre.'TYRES -> '.$cardet;
+	
+	} elseif ($lenght == 51) { // 29 characters >>> 51
+	
+	$title = $pretitlef.'RIMS AND '.$bestfittyre.'TYRES FIT '.$cardet;
+	
+	} elseif ($lenght == 50) { // 30 characters >>> 50
+	
+	$title = $pretitlef.'RIMS AND '.$bestfittyre.'TYRES FITS '.$cardet;
+	
+	} elseif ($lenght == 49) { // 31 characters >>> 49
+	
+	$title = $pretitlef.'WHEELS AND '.$bestfittyre.'TYRES FIT '.$cardet;
+	
+	} elseif ($lenght == 48) { // 32 characters >>> 48
+	
+	$title = $pretitlef.'WHEELS AND '.$bestfittyre.'TYRES FITS '.$cardet;
+	
+	} elseif (($lenght >= 46) && ($lenght <= 47)) { // 33-34 characters >>> 46-47
+	
+	$title = $pretitlef.'ALLOY WHEELS AND '.$bestfittyre.'TYRES '.$cardet;
+	
+	} elseif ($lenght == 45) { // 35 characters >>> 45
+	
+	$title = $pretitlef.'ALLOY WHEELS AND '.$bestfittyre.'TYRES - '.$cardet;
+	
+	} elseif ($lenght == 44) { // 36 characters >>> 44
+	
+	$title = $pretitlef.'ALLOY WHEELS AND '.$bestfittyre.'TYRES -> '.$cardet;
+	
+	} elseif ($lenght == 43) { // 37 characters >>> 43
+	
+	$title = $pretitlef.'ALLOY WHEELS AND '.$bestfittyre.'TYRES FIT '.$cardet;
+	
+	} elseif ($lenght == 42) { // 38 characters >>> 42
+	
+	$title = $pretitlef.'ALLOY WHEELS AND '.$bestfittyre.'TYRES FITS '.$cardet;
+	
+	} elseif ($lenght == 41) { // 39 characters >>> 41
+	
+	$title = $pretitlef.'ALLOY WHEELS AND '.$bestfittyre.'TYRES FIT > '.$cardet;
+	
+	} elseif ($lenght == 40) { // 40 characters >>> 40
+	
+	$title = $pretitlef.'ALLOY WHEELS AND '.$bestfittyre.'TYRES FIT -> '.$cardet;
+	
+	} elseif ($lenght == 39) { // 41 characters >>> 39
+	
+	$title = $pretitlef.'ALLOY WHEELS AND '.$bestfittyre.'TYRES FITS -> '.$cardet;
 	
 	} 
 	
